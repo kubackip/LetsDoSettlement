1b0a8b707e301ff98d1b25f9bfd461ef28e222dc
kubackip <przemekjerzykubacki@gmail.com>
2020-09-29 23:31:23 +0200

    First try to create listView, and set it visible.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module kubackip.github.io.LetsDoSettlement {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.base;
 
     opens kubackip.github.io.LetsDoSettlement to javafx.fxml;
     exports kubackip.github.io.LetsDoSettlement;
